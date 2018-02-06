22c9e6e426b9c66b941164386bc670a7c4646a79
Kysuke <Kysuke@192.168.1.66>
2018-02-06 23:12:56 +0100

    foodies-server - version -  wip

--- src/main/java/helper/constant/ConfigurationConstants.java
+++ src/main/java/helper/constant/ConfigurationConstants.java
@@ -7,7 +7,7 @@ public class ConfigurationConstants {
     public static final int jsonMsgDepth = 1;
     public static final int tokenSize = 20;
     public static final Environment environment = Environment.Debug;
-    public static final String version = "2.51c";
+    public static final String version = "1.01b";
     public static final String versionDate = "January 2018";
     public static final String author = "Ismael Garcia Lorenzo";
     public static final String authorMail = "Isgarlo87{at}gmail{dot}com";
